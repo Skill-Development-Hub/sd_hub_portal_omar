8c2703ee9eec8cbaef5471aa7572d75ef0a2ea39
umarshahbaz <umarshahbaz2000@gmail.com>
2024-10-03 12:18:59 +0530

    .

--- SD_Hub_Portal/src/app/app.module.ts
+++ SD_Hub_Portal/src/app/app.module.ts
@@ -4,18 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatStepperModule} from '@angular/material/stepper';
+import { MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { SigninComponent } from './signin/signin.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [
     provideAnimationsAsync()
